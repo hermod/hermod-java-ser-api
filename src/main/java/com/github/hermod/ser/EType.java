72512005fd2b3875aad5ac532b7177a8018c65a3
alexandrenavarro <alexandre.j.navarro@gmail.com>
2013-04-08 22:48:55 +0200

    Change EType constructor value

--- src/main/java/com/github/hermod/ser/EType.java
+++ src/main/java/com/github/hermod/ser/EType.java
@@ -8,8 +8,8 @@ package com.github.hermod.ser;
  */
 public enum EType {
 
-    NULL((byte) 0x0), MSG((byte) 0x20), INTEGER((byte) 0x40), DECIMAL((byte) 0x60), STRING_ISO_8859_1((byte) 0x80), STRING_UTF16((byte) 0xA0), FIXED_VALUE_LENGTH_ARRAY(
-            (byte) 0xC0), VARIABLE_VALUE_LENGTH_ARRAY((byte) 0xE0);
+    NULL((byte) 0b0000_0000), MSG((byte) 0b0010_0000), INTEGER((byte) 0b0100_0000), DECIMAL((byte) 0b0110_0000), STRING_ISO_8859_1((byte) 0b1000_0000), STRING_UTF16((byte) 0b1010_0000), FIXED_VALUE_LENGTH_ARRAY(
+            (byte) 0b1100_0000), VARIABLE_VALUE_LENGTH_ARRAY((byte) 0b1110_0000);
 
     private final byte id;
 
@@ -27,7 +27,7 @@ public enum EType {
      *
      * @return
      */
-    public int getId() {
+    public byte getId() {
         return this.id;
     }
 
