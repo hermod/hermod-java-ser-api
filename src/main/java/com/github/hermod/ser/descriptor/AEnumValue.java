a2636b708b4bc5a2003a3d8299f08dc3204f761f
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2013-04-26 11:10:00 +0300

    Update AEnumValue.java

--- src/main/java/com/github/hermod/ser/descriptor/AEnumValue.java
+++ src/main/java/com/github/hermod/ser/descriptor/AEnumValue.java
@@ -13,7 +13,7 @@ import java.lang.annotation.Target;
  *
  */
 @Retention(RetentionPolicy.RUNTIME) 
-@Target({ElementType.METHOD}) 
+@Target({ElementType.FIELD}) 
 @Documented
 public @interface AEnumValue {
 
