43041874e5c7a1c169e7cee55c02410ae5bd6ff6
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2013-09-16 17:19:53 +0200

    Update ByteBufferSerializable.java

--- src/main/java/com/github/hermod/ser/ByteBufferSerializable.java
+++ src/main/java/com/github/hermod/ser/ByteBufferSerializable.java
@@ -5,26 +5,72 @@ import java.nio.ByteBuffer;
 
 
 /**
- * <p>IByteBufferSerializable. </p>
+ * <p>IByteBufferClassSerializer. </p>
  *
  * @author anavarro - Apr 13, 2013
+ * @param <T>
  *
  */
-public interface ByteBufferSerializable extends Serializable {
+/**
+ * <p>IByteBufferClassSerializer. </p>
+ *
+ * @author anavarro - Apr 20, 2013
+ * @param <T>
+ *
+ */
+/**
+ * <p>IByteBufferClassSerializer. </p>
+ *
+ * @author anavarro - Apr 20, 2013
+ * @param <T>
+ *
+ */
+public interface ByteBufferClassSerializer<T> {
 
     /**
      * serializeToByteBuffer.
      *
+     * @param aSrcObjet
+     * @return
+     */
+    ByteBuffer serializeToByteBuffer(final T aSrcObjet);
+    
+    /**
+     * serializeToByteBuffer.
+     *
+     * @param aSrcObjet
      * @param aDestByteBuffer
      */
-    void serializeToByteBuffer(final ByteBuffer aDestByteBuffer);
-
+    void serializeToByteBuffer(final T aSrcObjet, final ByteBuffer aDestByteBuffer);
     
     /**
      * deserializeFrom.
      *
      * @param aSrcByteBuffer
+     * @param offset
+     * @param aSrcLength
+     * @param aDestObject
      */
-    void deserializeFromByteBuffer(final ByteBuffer aSrcByteBuffer);
+    void deserializeFromByteBuffer(final ByteBuffer aSrcByteBuffer, final int aSrcLength, T aDestObject);
+   
+    /**
+     * deserializeFrom.
+     *
+     * @param srcByteBuffer
+     * @param srcLength
+     * @return
+     */
+    T deserializeFromByteBuffer(final ByteBuffer srcByteBuffer, final int srcLength);
+    
+    /**
+     * deserializeFrom.
+     *
+     * @param srcByteBuffer
+     * @param srcLength
+     * @param aClass
+     * @return
+     */
+    <S> S deserializeFromByteBuffer(final ByteBuffer srcByteBuffer, final int srcLength, final Class<S> aClass);
+    
     
 }
