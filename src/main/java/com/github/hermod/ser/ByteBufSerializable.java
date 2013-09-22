4c07373812cf0f68254f25e3ea2336168c649d1f
alexandrenavarro <alexandre.j.navarro@gmail.com>
2013-09-22 23:28:11 +0200

    Fix pb with ByteBufSerializable

--- src/main/java/com/github/hermod/ser/ByteBufSerializable.java
+++ src/main/java/com/github/hermod/ser/ByteBufSerializable.java
@@ -1,71 +1,60 @@
 package com.github.hermod.ser;
 
+import io.netty.buffer.ByteBuf;
 
 import java.nio.ByteBuffer;
 
-import com.github.hermod.ser.Serializable;
-
 
 
 /**
- * <p>IByteBufferClassSerializer. </p>
- *
- * @author anavarro - Apr 13, 2013
- * @param <T>
- *
- */
+* <p>IByteBufferClassSerializer. </p>
+*
+* @author anavarro - Apr 13, 2013
+* @param <T>
+*
+*/
 /**
- * <p>IByteBufferClassSerializer. </p>
- *
- * @author anavarro - Apr 20, 2013
- * @param <T>
- *
- */
+* <p>IByteBufferClassSerializer. </p>
+*
+* @author anavarro - Apr 20, 2013
+* @param <T>
+*
+*/
 /**
- * <p>IByteBufferClassSerializer. </p>
- *
- * @author anavarro - Apr 20, 2013
- * @param <T>
- *
- */
-public interface ByteBufferSerializable extends Serializable {
+* <p>IByteBufferClassSerializer. </p>
+*
+* @author anavarro - Apr 20, 2013
+* @param <T>
+*
+*/
+public interface ByteBufSerializable extends Serializable {
 
     /**
-     * serializeToByteBuffer.
-     *
-     * @param aSrcObjet
-     * @return
-     */
-    ByteBuffer serializeToByteBuffer();
+* serializeToByteBuffer.
+*
+* @param aSrcObjet
+* @return
+*/
+    ByteBuf serializeToByteBuf();
     
     /**
-     * serializeToByteBuffer.
-     *
-     * @param aSrcObjet
-     * @param aDestByteBuffer
-     */
-    void serializeToByteBuffer(final ByteBuffer aDestByteBuffer);
+* serializeToByteBuffer.
+*
+* @param aSrcObjet
+* @param aDestByteBuffer
+*/
+    void serializeToByteBuf(final ByteBuf aDestByteBuf);
     
     /**
-     * deserializeFrom.
-     *
-     * @param aSrcByteBuffer
-     * @param offset
-     * @param aSrcLength
-     * @param aDestObject
-     */
-    void deserializeFromByteBuffer(final ByteBuffer aSrcByteBuffer);
+* deserializeFrom.
+*
+* @param aSrcByteBuffer
+* @param offset
+* @param aSrcLength
+* @param aDestObject
+*/
+    void deserializeFromByteBuf(final ByteBuf aSrcByteBuf);
    
-    
-    /**
-     * deserializeFrom.
-     *
-     * @param srcByteBuffer
-     * @param srcLength
-     * @param aClass
-     * @return
-     */
-    //<S> S deserializeFromByteBuffer(final ByteBuffer srcByteBuffer, final Class<S> aClass);
     
     
 }
