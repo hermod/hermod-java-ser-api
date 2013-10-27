59b4d133f0af7d4487f978f2c4167f568d61e119
alexandrenavarro <alexandre.j.navarro@gmail.com>
2013-10-27 05:30:41 +0100

    Add in Types Type valueOf(Class clazz);

--- src/main/java/com/github/hermod/ser/Type.java
+++ src/main/java/com/github/hermod/ser/Type.java
@@ -55,5 +55,30 @@ public enum Type {
         }
         throw new IllegalArgumentException("The type with id=" + aId + " not found in EType.values()=" + Arrays.asList(Type.values()));
     }
-
+    
+    /**
+     * valueOf.
+     *
+     * @param clazz
+     * @return
+     */
+    public static <T> Type valueOf(final Class<T> clazz) {
+        //TODO to optimize Type valueOf(final String className) and a switch
+        if (Integer.class.equals(clazz) || Long.class.equals(clazz) || Short.class.equals(clazz) || Byte.class.equals(clazz) || Boolean.class.equals(clazz)) {
+            return Type.INTEGER;
+        } else if (Double.class.equals(clazz) || Float.class.equals(clazz)) {
+            return Type.DECIMAL;
+        } else if (String.class.equals(clazz)) {
+            // TODO change on Type.STRING_UTF8
+            return Type.STRING_ISO_8859_1;
+        } else if (Msg.class.equals(clazz)) {
+            return Type.MSG;
+        } else if (int[].class.equals(clazz) || long[].class.equals(clazz) || short[].class.equals(clazz) || byte[].class.equals(clazz) || boolean[].class.equals(clazz) || double[].class.equals(clazz) || float[].class.equals(clazz)) {
+            return Type.ARRAY_FIXED_VALUE;
+        } else if (Integer[].class.equals(clazz) || Long[].class.equals(clazz) || Short[].class.equals(clazz) || Byte[].class.equals(clazz) || Boolean[].class.equals(clazz) || Double[].class.equals(clazz) || Float[].class.equals(clazz) || String[].class.equals(clazz) || Msg[].class.equals(clazz)) {
+            return Type.ARRAY_VARIABLE_VALUE;
+        } 
+        throw new IllegalArgumentException("The type with class=" + clazz + " not found in EType.values()=" + Arrays.asList(Type.values()));
+    }
+    
 }
